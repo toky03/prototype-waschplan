2bed7ccdae3594710b8c487b293cf3710fef3f2d
Marco <marco.jakob3@gmail.com>
2021-01-10 13:47:10 +0100

    Korrigierte kalenderfunktion

---
+++ waschplan-frontend/src/components/Termine.tsx
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+
+const Termine = () => {
+  const termine = useSelector((state: any) => state.termine);
+  return (
+    <ul>
+      {termine.map((termin: any) => <li  key={termin.id}>{termin.name} {termin.date}</li>)}
+    </ul>
+  )
+};
+  
+export default Termine;
